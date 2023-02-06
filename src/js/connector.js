9813166a40f16afcc95e4d8f97d3ef53b3709d70
Alireza Ahmadi <alirezaa@spotify.com>
2023-02-06 14:51:37 +0100

    Add missing gray icon

--- src/js/connector.js
+++ src/js/connector.js
@@ -116,7 +116,9 @@ import { defaultUnitForLocale, celsiusToFahrenheit, kphToMph } from './modules/u
 //   }
 // );
 
-var onBtnClick = function (t, opts) {
+var GRAY_ICON = 'https://cdn.hyperdev.com/us-east-1%3A3d31b21c-01a0-4da2-8827-4bc6e88b7618%2Ficon-gray.svg';
+
+var onBtnClick = function (t, opts) {a
   console.log('Someone clicked the button');
 };
 
